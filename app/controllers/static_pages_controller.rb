f9451a732075309fa2a3daed9df519094bb8fb18
dvt <trinhvideo123@gmail.com>
2022-03-21 12:06:58 +0700

    Change Home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @trending_pagy, @trending_products = pagy(Product.trending,
-                                              items: Settings.item.max_item_10)
-
-    @new_pagy, @new_products = pagy(Product.newest,
-                                    items: Settings.item.max_item_10)
+    @type = params[:type]&.to_sym || :newest
+    @pagy, @products = pagy Product.try(@type)
   end
 end
